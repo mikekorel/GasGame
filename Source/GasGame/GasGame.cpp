3f714ad41c729dd58de68efd498f7063b748e8ab
Mike Korelidis <kore.mike@gmail.com>
2025-08-13 18:37:37 +0300

    Initial project files

---
+++ Source/GasGame/GasGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GasGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GasGame, "GasGame" );
