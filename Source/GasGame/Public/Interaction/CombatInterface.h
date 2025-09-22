08de81c722b4bc3b01ebff862b86a014a14a36f0
Mike Korelidis <kore.mike@gmail.com>
2025-09-22 17:07:17 +0300

    Add Motion Warping, use to rotate character when casting a spell

--- Source/GasGame/Public/Interaction/CombatInterface.h
+++ Source/GasGame/Public/Interaction/CombatInterface.h
@@ -4,7 +4,7 @@
 #include "UObject/Interface.h"
 #include "CombatInterface.generated.h"
 
-UINTERFACE(MinimalAPI)
+UINTERFACE(MinimalAPI, BlueprintType)
 class UCombatInterface : public UInterface
 {
 	GENERATED_BODY()
@@ -15,6 +15,9 @@ class GASGAME_API ICombatInterface
 	GENERATED_BODY()
 
 public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void UpdateFacingTarget(const FVector& Target);
+	
 	virtual int32 GetPlayerLevel();
 	virtual FVector GetCombatSocketLocation();
 };
