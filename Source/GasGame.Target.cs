16c89abb63c75ded07c62d3e95ce5ba8e22d7bf2
Mike Korelidis <kore.mike@gmail.com>
2025-11-17 23:06:27 +0200

    Migrate to Unreal 5.7

--- Source/GasGame.Target.cs
+++ Source/GasGame.Target.cs
@@ -8,7 +8,7 @@ public class GasGameTarget : TargetRules
 	public GasGameTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("GasGame");
 	}
